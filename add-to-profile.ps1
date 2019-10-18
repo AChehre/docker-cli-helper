490869949016f63a771bd61c3ea8f9292d5011f4
ahmad <achehre@yahoo.com>
2019-10-18 20:28:11 +0330

    create powershell profile file if it does not exist

--- add-to-profile.ps1
+++ add-to-profile.ps1
@@ -1,13 +1,15 @@
-Write-Output "`nProfile path is:"
-Write-Output "`n----------------------------------`n"
-Write-Output $profile 
-Write-Output "`n=================================="
+Write-Host "`n#> " -ForegroundColor DarkGreen -NoNewline;  Write-Host "Profile path is:`n"
+Write-Host $profile 
+
+if (!(Test-Path $profile))
+{
+   New-Item -Path $profile -ItemType File -Force
+}
+    
 
 $From = Get-Content -Path .\docker-helper-methods.ps1
 Add-Content -Path $profile -Value $From
 
-Write-Output "`nCurrent profile content is:"
-Write-Output "`n----------------------------------`n"
+Write-Host "`n>> " -ForegroundColor DarkGreen -NoNewline; Write-Host "Current profile content is:`n"
 Get-Content -Path $profile
-Write-Output "`n==================================`n"
-Write-Output "Done!`n"
+Write-Host "`n<< " -ForegroundColor DarkGreen -NoNewline; Write-Host "Done! `n"
